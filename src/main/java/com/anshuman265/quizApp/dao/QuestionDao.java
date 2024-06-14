74bc0640bc095ee3ae3b0270a86c66cccdea1134
Anshuman <verma77712@gmail.com>
2024-06-14 21:02:16 +0530

    Dao learned

--- src/main/java/com/anshuman265/quizApp/dao/QuestionDao.java
+++ src/main/java/com/anshuman265/quizApp/dao/QuestionDao.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+// Data object which will be used to retried questions
 @Repository
 public interface QuestionDao extends JpaRepository<Question,Integer> {
     List<Question> findByCategory(String category);
