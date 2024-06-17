d2f66af2daa01e2eb2ffbe99b59d13253ae5e073
Anshuman <verma77712@gmail.com>
2024-06-17 22:54:21 +0530

    controller added

---
+++ src/main/java/com/anshuman265/quizApp/controller/QuizController.java
@@ -0,0 +1,17 @@
+package com.anshuman265.quizApp.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("quiz")
+public class QuizController {
+
+    @PostMapping("create")
+    public ResponseEntity<String> createQuiz(){
+
+    }
+}
