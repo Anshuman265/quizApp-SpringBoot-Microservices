8525ff4d079c1a4af27b5ee9fa24d66873a7d3dc
Anshuman <verma77712@gmail.com>
2024-06-18 17:08:39 +0530

    quizController

--- src/main/java/com/anshuman265/quizApp/controller/QuizController.java
+++ src/main/java/com/anshuman265/quizApp/controller/QuizController.java
@@ -1,9 +1,11 @@
 package com.anshuman265.quizApp.controller;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class QuizController {
 
     @PostMapping("create")
-    public ResponseEntity<String> createQuiz(){
-
+    public ResponseEntity<String> createQuiz(
+            @RequestParam String category,
+            @RequestParam int numQ,
+            @RequestParam String title
+    ){
+        return new ResponseEntity<>("I'm here", HttpStatus.OK)
     }
 }
